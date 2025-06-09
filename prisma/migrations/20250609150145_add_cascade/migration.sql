252a203768a36d12358cedd0b42345d1a4f7db3d
John-Rashta <john_rashta@outlook.com>
2025-06-09 16:02:14 +0100

    fix: add cascade

---
+++ prisma/migrations/20250609150145_add_cascade/migration.sql
@@ -0,0 +1,5 @@
+-- DropForeignKey
+ALTER TABLE "Comment" DROP CONSTRAINT "Comment_commentid_fkey";
+
+-- AddForeignKey
+ALTER TABLE "Comment" ADD CONSTRAINT "Comment_commentid_fkey" FOREIGN KEY ("commentid") REFERENCES "Comment"("id") ON DELETE CASCADE ON UPDATE CASCADE;
